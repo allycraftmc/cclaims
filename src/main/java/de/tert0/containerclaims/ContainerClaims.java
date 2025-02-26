3a79ce8b8f2d533e21ce7b7fc3dfdf735d12023b
Tert0 <tert0byte@gmail.com>
2025-02-26 14:09:25 +0100

    initial commit

---
+++ src/main/java/de/tert0/containerclaims/ContainerClaims.java
@@ -0,0 +1,10 @@
+package de.tert0.containerclaims;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ContainerClaims implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
