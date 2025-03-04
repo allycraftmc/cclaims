a758a7b0ac19adc97e247963f995961fcc9b91b8
Tert0 <tert0byte@gmail.com>
2025-03-04 17:47:36 +0100

    prevent extracting with hoppers or hopper minecarts and allow claiming hoppers

---
+++ src/main/java/de/tert0/containerclaims/mixin/HopperBlockEntityMixin.java
@@ -0,0 +1,38 @@
+package de.tert0.containerclaims.mixin;
+
+import de.tert0.containerclaims.ClaimAccess;
+import de.tert0.containerclaims.ClaimUtils;
+import net.minecraft.block.entity.Hopper;
+import net.minecraft.block.entity.HopperBlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HopperBlockEntity.class)
+public abstract class HopperBlockEntityMixin {
+    @Inject(
+            method = "extract(Lnet/minecraft/world/World;Lnet/minecraft/block/entity/Hopper;)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/entity/HopperBlockEntity;getAvailableSlots(Lnet/minecraft/inventory/Inventory;Lnet/minecraft/util/math/Direction;)[I"),
+            cancellable = true
+    )
+    private static void extract(World world, Hopper hopper, CallbackInfoReturnable<Boolean> cir) {
+        BlockPos blockPos = BlockPos.ofFloored(hopper.getHopperX(), hopper.getHopperY() + 1.0, hopper.getHopperZ());
+        ClaimAccess claimAccess = (ClaimAccess) world.getBlockEntity(blockPos);
+        if(claimAccess != null && ClaimUtils.isClaimed(claimAccess)) {
+            if(hopper instanceof HopperBlockEntity hopperBlockEntity) {
+                ClaimAccess hopperClaimAccess = (ClaimAccess) hopperBlockEntity;
+                if(
+                        !ClaimUtils.isClaimed(hopperClaimAccess)
+                                || (!claimAccess.cclaims$getClaim().owner().equals(hopperClaimAccess.cclaims$getClaim().owner()) && !claimAccess.cclaims$getClaim().trusted().contains(hopperClaimAccess.cclaims$getClaim().owner()))
+                ) {
+                    cir.setReturnValue(false);
+                }
+            } else {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
