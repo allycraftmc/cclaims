c8543658aeb4ccf7fe86a3897ee60782e45ea82a
Tert0 <tert0byte@gmail.com>
2025-03-04 23:33:22 +0100

    fix desync on double chest place prevention

--- src/main/java/de/tert0/containerclaims/mixin/BlockItemMixin.java
+++ src/main/java/de/tert0/containerclaims/mixin/BlockItemMixin.java
@@ -5,9 +5,11 @@ import de.tert0.containerclaims.ClaimUtils;
 import de.tert0.containerclaims.DoubleChestUtils;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
@@ -24,11 +26,17 @@ public abstract class BlockItemMixin {
     @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;Lnet/minecraft/block/BlockState;)Z", at = @At("HEAD"), cancellable = true)
     void place(ItemPlacementContext context, BlockState state, CallbackInfoReturnable<Boolean> cir) {
         if(context.getPlayer() == null) return; // TODO
+        ServerPlayerEntity player = (ServerPlayerEntity) context.getPlayer();
 
         ClaimAccess claimAccess = (ClaimAccess) DoubleChestUtils.getNeighborBlockEntity(context.getBlockPos(), context.getWorld(), state);
         if(claimAccess != null && ClaimUtils.isClaimed(claimAccess)) {
-            if(!ClaimUtils.isOwnerOrAdmin(claimAccess, context.getPlayer())) {
-                context.getPlayer().sendMessage(Text.literal("The other chest is claimed!").withColor(Colors.RED), false);
+            if(!ClaimUtils.isOwnerOrAdmin(claimAccess, player)) {
+                player.sendMessage(Text.literal("The other chest is claimed!").withColor(Colors.RED), true);
+                int slot = switch (context.getHand()) {
+                    case MAIN_HAND -> player.getInventory().selectedSlot;
+                    case OFF_HAND -> PlayerInventory.OFF_HAND_SLOT;
+                };
+                player.networkHandler.sendPacket(player.getInventory().createSlotSetPacket(slot));
                 cir.setReturnValue(false);
             }
         }
