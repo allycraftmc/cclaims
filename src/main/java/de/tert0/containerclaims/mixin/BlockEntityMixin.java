7da06a9fc228b9fad77e6fbaa4eeb68402769ee0
Tert0 <tert0byte@gmail.com>
2025-03-06 18:21:13 +0100

    improve tracking global claim state positions

--- src/main/java/de/tert0/containerclaims/mixin/BlockEntityMixin.java
+++ src/main/java/de/tert0/containerclaims/mixin/BlockEntityMixin.java
@@ -3,9 +3,14 @@ package de.tert0.containerclaims.mixin;
 import de.tert0.containerclaims.ClaimComponent;
 import de.tert0.containerclaims.ClaimAccess;
 import de.tert0.containerclaims.ContainerClaimMod;
+import de.tert0.containerclaims.GlobalClaimState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.*;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,6 +27,10 @@ public abstract class BlockEntityMixin implements ClaimAccess {
 
     @Shadow @Final private static Logger LOGGER;
 
+    @Shadow @Nullable public abstract World getWorld();
+
+    @Shadow public abstract BlockPos getPos();
+
     @Unique
     private ClaimComponent claim;
 
@@ -32,6 +41,9 @@ public abstract class BlockEntityMixin implements ClaimAccess {
                 .resultOrPartial(LOGGER::error)
                 .orElseThrow();
         nbt.put(ContainerClaimMod.CLAIM_DATA_ID.toString(), nbtClaim);
+
+        // to track claimed containers that were not directly claimed through the mod (e.g. modifying nbt or cloning a block entity)
+        GlobalClaimState.getWorldState((ServerWorld) this.getWorld()).addPosition(this.getPos());
     }
 
     @Inject(method = "readNbt", at = @At("RETURN"))
